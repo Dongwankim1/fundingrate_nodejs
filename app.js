5ee971a3da2d8716b17ebd53041d8c5dc3ab2210
dongwan <zkdlwu94@naver.com>
2021-11-03 23:26:28 +0900

    aa

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+const puppeteer = require("puppeteer");
+const cheerio = require('cheerio');
+var db = mongoose.connection;
+db.on('error',console.error);
+db.once('open',function(){
+  console.log("Connected to mongodb server")
+});
+
+mongoose.connect('mongodb://localhost:27017/db_fundingrate');
+var rate = require('./models/rate');
+
+  (async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1366,
+      height: 768
+    });
+    // "https://www.goodchoice.kr/product/search/2" URL에 접속한다. (여기어때 호텔 페이지)
+    await page.goto("https://www.coinglass.com/FundingRate");
+    
+    
+    const content = await page.content();
+
+    const $ = cheerio.load(content);
+
+    const lists = $("#ufr");
+    var ratemodel = new rate();
+    var nIndex = 0;
+    var column = ['binance','Okex','Bybit','FTX','Huobi','Gate','Bitget'];
+    for(let i =0;i < lists[0].children[0].children.length;i++){
+        var node = lists[0].children[0].children[i]
+        var data = node.children[0].childNodes[0].children[0].data;
+        ratemodel[column[nIndex++]]=data;
+    }
+
+   
+    
+    ratemodel.save(function(err){
+      if(err){
+          console.error(err);
+
+          return;
+      }
+
+
+  });
+    
+    browser.close();
+    
+  })();
+  
---
+++ models/rate.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var rateSchema = new Schema({
+    binance:String,
+    Okex:String,
+    Bybit:String,
+    FTX:String,
+    Huobi:String,
+    Gate:String,
+    Bitget:String,
+    frstDate:{type:Date,default:new Date.now}
+
+})
+
+
+module.exports = mongoose.model('rate',rateSchema)
